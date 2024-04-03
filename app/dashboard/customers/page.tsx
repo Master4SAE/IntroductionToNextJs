59b4e6f5b4162758f8fabf0e4bcfb25246dffaf6
Sohaib_School <sohaibe@metropolia.fi>
2024-04-03 17:11:12 +0300

    Customers page updated

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-    return<p>Customers Page</p>;
-  }
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import Search from '@/app/ui/search';
+
+export default async function Page( {
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customertable = await fetchFilteredCustomers(query)
+  const currentPage = Number(searchParams?.page) || 1;
+
+  return (
+    <div className="w-full">
+      <Table customers={customertable}></Table>
+    </div>
+  );
+}
